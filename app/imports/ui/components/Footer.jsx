4090d9ffd3239a61e5445906372c904ad7144c85
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2019-11-13 19:01:44 -1000

    Landing

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Input, Label } from 'semantic-ui-react';
+
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
     const divStyle = { paddingTop: '15px' };
     return (
         <footer>
-          <div style={divStyle} className="ui center aligned container">
-
-            <hr />
-            Welcome back! Let's get creative! <br />
-            <Input focus placeholder='Email or Username' /> <br />
-            <Input focus placeholder='Password' /> <br />
-            <Label color="black">Log in</Label> <br />
-
-          </div>
         </footer>
     );
   }
